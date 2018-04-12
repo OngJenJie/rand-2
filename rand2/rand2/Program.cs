ebf56c2f99d56db0193c39f9780f2be4be0902ea
CancerNeko <jenjieong@yahoo.com>
2018-04-12 17:45:38 +0800

    Done

---
+++ rand2/rand2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rand2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 1; i<=42; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Random rand = new Random();
+            for(int i = 0; i <6; i++)
+            {
+                int r = rand.Next(1, numbers.Count);
+                Console.Write(numbers[r] + " ");
+                numbers.RemoveAt(r);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
