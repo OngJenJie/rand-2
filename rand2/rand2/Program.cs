b16ee72a4e17d440bd8f6648fa1ea9ea22b0984f
CancerNeko <jenjieong@yahoo.com>
2018-04-19 22:40:54 +0800

    加入了註解

--- rand2/rand2/Program.cs
+++ rand2/rand2/Program.cs
@@ -10,13 +10,20 @@ namespace rand2
     {
         static void Main(string[] args)
         {
+
+            // 創造list
             List<int> numbers = new List<int>();
+
+            // 加入數字
             for (int i = 1; i<=42; i++)
             {
                 numbers.Add(i);
             }
 
+            // 宣告隨機亂數
             Random rand = new Random();
+
+            //輸出留個數字 并從list 移除。
             for(int i = 0; i <6; i++)
             {
                 int r = rand.Next(1, numbers.Count);
