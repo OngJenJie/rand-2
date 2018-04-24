81c92df37dfef6c83b29b2b8815612b68167dc21
CancerNeko <jenjieong@yahoo.com>
2018-04-24 20:23:04 +0800

    優化使用者操作方式 以及 加入了註解

--- rand2/rand2/Program.cs
+++ rand2/rand2/Program.cs
@@ -14,8 +14,19 @@ namespace rand2
             // 創造list
             List<int> numbers = new List<int>();
 
+            // 讓使用者輸入最高跟最低的亂數限制
+            Console.WriteLine("請輸入最低的亂數限制: ");
+            int mincount = int.Parse(Console.ReadLine());
+            Console.WriteLine("請輸入最高的亂數限制: ");
+            int maxcount = int.Parse(Console.ReadLine());
+
+            // 讓使用者輸入需要多少的亂數
+            Console.WriteLine("請輸入需要多少亂數：");
+            int randcount = int.Parse(Console.ReadLine());
+            Console.WriteLine("▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆我是分割線▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆");
+
             // 加入數字
-            for (int i = 1; i<=42; i++)
+            for (int i = mincount; i<=maxcount; i++)
             {
                 numbers.Add(i);
             }
@@ -23,8 +34,8 @@ namespace rand2
             // 宣告隨機亂數
             Random rand = new Random();
 
-            //輸出留個數字 并從list 移除。
-            for(int i = 0; i <6; i++)
+            // 輸出留個數字 并從list 移除。
+            for(int i = 0; i <randcount; i++)
             {
                 int r = rand.Next(1, numbers.Count);
                 Console.Write(numbers[r] + " ");
